e5c8e07a0c6e31ed6f2cda01883320aa4c0ed30f
Hayden <hayden.barnes@suse.com>
2022-05-17 11:45:30 -0400

    Enable optional dark mode theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
       },
       colorMode: {
         defaultMode: 'light',
-        disableSwitch: true,
+        disableSwitch: false,
       },
       footer: {
         style: 'dark',
